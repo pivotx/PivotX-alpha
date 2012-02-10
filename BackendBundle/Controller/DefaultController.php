19e9e55e58da40b73a9bbefcbba5260955a1b686
Bob den Otter <bob@pivotx.net>
2012-02-10 13:51:33 +0100

    Use annotation for BackendBundle.

--- BackendBundle/Controller/DefaultController.php
+++ BackendBundle/Controller/DefaultController.php
@@ -3,11 +3,15 @@
 namespace PivotX\BackendBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
-
+    /**
+     * @Route("/hello/{name}")
+     * @Template()
+     */
     public function indexAction($name)
     {
         return $this->render('PivotXBackendBundle:Default:index.html.twig', array('name' => $name));
